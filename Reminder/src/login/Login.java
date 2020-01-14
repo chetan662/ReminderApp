c5d553be0abc5c57c29aaf5dcf4805b3abd58316
plutothepenguin <43420295+plutothepenguin@users.noreply.github.com>
2020-01-14 10:09:56 -0500

    Update Login.java

--- Reminder/src/login/Login.java
+++ Reminder/src/login/Login.java
@@ -1,23 +1,21 @@
 package login;
-
-import com.sun.javafx.tk.Toolkit;
-import java.awt.SystemTray;
-import java.awt.TrayIcon;
-import java.awt.TrayIcon.MessageType;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import javax.swing.JTextField;
+
 public class Login extends Application {
-    
     public void start(Stage primaryStage) {
+        int counter = 0;
+        int[][] remindersArray = new int[2][100];
         primaryStage.setTitle("Home");
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
         GridPane grid = new GridPane();
@@ -25,59 +23,30 @@ public class Login extends Application {
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(5);
-        Button gotoLogin = new Button("Set reminder for 5 seconds");
-        grid.add(gotoLogin, 0, 0);
+        Button addButton = new Button("+");
+        TextField reminderIn = new TextField("reminder");
+        TextField hour = new TextField();
+        TextField minute = new TextField();
+        Text reminders = new Text("");
+        grid.add(reminderIn, 0, 0);
+        grid.add(hour, 1, 0);
+        grid.add(minute, 2, 0);
+        grid.add(addButton, 2, 0);
+        grid.add(reminders, 0, 4);
         primaryStage.setScene(scene);
         primaryStage.show();
-        
-        
-        
-        gotoLogin.setOnAction((ActionEvent event) -> {
-            try {
-                TimeUnit.SECONDS.sleep(5);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            System.out.println("5 secs done");
-            primaryStage.setTitle("Login");
-            grid.getChildren().remove(gotoLogin);
+        addButton.setOnAction((ActionEvent event) -> {
+            System.out.println(remindersArray.length);
+            reminders.setText(reminders.getText() + "\n" + reminderIn.getText() + " at " + hour.getText() + minute.getText());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            counter++;
         });
+        
+        
     }
     
     public static void main(String[] args) {
-        
-//        try{
-//            //Obtain only one instance of the SystemTray object
-//            SystemTray tray = SystemTray.getSystemTray();
-//
-//            // If you want to create an icon in the system tray to preview 
-//            try {
-//                Image retValue = Toolkit.getToolkit().getImage(ClassLoader.getSystemResource("icon.png"));
-//                
-//            }
-//            catch (Exception e) {
-//                
-//            }
-////            Image image = Toolkit.getDefaultToolkit().createImage("some-icon.png");
-//            //Alternative (if the icon is on the classpath):
-//            //Image image = Toolkit.getDefaultToolkit().createImage(getClass().getResource("icon.png"));
-//
-//            TrayIcon trayIcon = new TrayIcon(image, "Java AWT Tray Demo");
-//            //Let the system resize the image if needed
-//            trayIcon.setImageAutoSize(true);
-//            //Set tooltip text for the tray icon
-//            trayIcon.setToolTip("System tray icon demo");
-//            tray.add(trayIcon);
-//
-//            // Display info notification:
-//            trayIcon.displayMessage("Hello, World", "Java Notification Demo", MessageType.INFO);
-//            // Error:
-//            // trayIcon.displayMessage("Hello, World", "Java Notification Demo", MessageType.ERROR);
-//            // Warning:
-//            // trayIcon.displayMessage("Hello, World", "Java Notification Demo", MessageType.WARNING);
-//        }catch(Exception ex){
-//            System.err.print(ex);
-//        }
         launch(args);
     }
     
