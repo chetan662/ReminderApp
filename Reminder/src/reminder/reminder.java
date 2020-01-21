4b0b5884fa3aa013870ca8b0e290c89dbc847b3a
omidghafori <cheeesebrain@gmail.com>
2020-01-21 00:55:39 -0500

    got sound working, nothing else

--- Reminder/src/reminder/reminder.java
+++ Reminder/src/reminder/reminder.java
@@ -1,7 +1,7 @@
 package reminder;
+import java.io.File;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
@@ -14,23 +14,25 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.text.Text;
+import java.util.ArrayList;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class reminder extends Application {
     public int counter = 0;
     public void start(Stage primaryStage) {
-        
-        String[] remindersArr = new String[100];
-        int[] hoursArr = new int[100];
-        int[] minutesArr = new int[100];
+        ArrayList<String> remindersArr = new ArrayList<>();
+        ArrayList<Integer> hoursArr = new ArrayList<>();
+        ArrayList<Integer> minutesArr = new ArrayList<>();
         primaryStage.setTitle("Home");
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
         GridPane grid = new GridPane();
         Scene scene = new Scene(grid, 888, 500);
-        
         grid.setAlignment(Pos.TOP_CENTER);
         grid.setHgap(5);
         grid.setVgap(5);
         Button addButton = new Button("+");
+        Button confirmButton = new Button("OK");
         TextField reminderIn = new TextField("reminder");
         Label errors = new Label("");
         TextField hour = new TextField();
@@ -39,8 +41,8 @@ public class reminder extends Application {
         Text colon = new Text(":");
         Text clock = new Text("");
         reminderIn.setPrefWidth(400);
-        hour.setPrefWidth(30);
-        minute.setPrefWidth(30);
+        hour.setPrefWidth(40);
+        minute.setPrefWidth(40);
         grid.add(clock, 0, 0);
         grid.add(errors, 0, 2);
         grid.add(reminderIn, 0, 1);
@@ -51,17 +53,30 @@ public class reminder extends Application {
         grid.add(reminders, 0, 3);
         Timer timer = new Timer();
         TimerTask repet = new TimerTask() {
-            @Override
+            
             public void run() {
                 String text = "";
+                long x = System.currentTimeMillis();
                 for (int i = 0; i < counter; i++) {
-                    
-                    text = String.format("%s%s at %02d:%02d%n", text, remindersArr[i], hoursArr[i], minutesArr[i]);
+                    if (remindersArr.size() > 0) {
+                        text = String.format("%s%s at %02d:%02d%n", text, remindersArr.get(i), hoursArr.get(i), minutesArr.get(i));
+                        if ((int) (x/(1000*60*60)) % 24 + 7 == hoursArr.get(i) && (int) (x/(1000*60)) % 60 == minutesArr.get(i)) {
+                            String musicFile = "sound.mp3";
+                            Media sound = new Media(new File(musicFile).toURI().toString());
+                            MediaPlayer mediaPlayer = new MediaPlayer(sound);
+                            mediaPlayer.play();
+                            remindersArr.remove(i);
+                            hoursArr.remove(i);
+                            minutesArr.remove(i);
+                            System.out.println("REMINDER");
+                        }
+                        else {
+                            text = "";
+                        }
+                    }
                 }
                 reminders.setText(text);
-                long millis = System.currentTimeMillis();
-                
-                clock.setText(String.format("%02d:%02d:%02d", (int) (millis/(1000*60*60)) % 24 + 7, (int) (millis/(1000*60)) % 60, (int) (millis/1000) % 60));
+                clock.setText(String.format("%02d:%02d:%02d", (int) (x/(1000*60*60)) % 24 + 7, (int) (x/(1000*60)) % 60, (int) (x/1000) % 60));
             }
         };
         timer.schedule(repet, 1000, 1000);
@@ -78,25 +93,17 @@ public class reminder extends Application {
                 errors.setText("Minute out of range");
             }
             else {
-                remindersArr[counter] = reminderIn.getText();
-                hoursArr[counter] = Integer.parseInt(hour.getText());
-                minutesArr[counter] = Integer.parseInt(minute.getText());
-                System.out.println(System.currentTimeMillis());
-                System.out.println(hoursArr[counter]*3600000+minutesArr[counter]*60000);
+                remindersArr.add(reminderIn.getText());
+                hoursArr.add(Integer.parseInt(hour.getText()));
+                minutesArr.add(Integer.parseInt(minute.getText()));
                 errors.setText("");
-                
                 counter++;
             }
             primaryStage.setScene(scene);
             primaryStage.show();
-            
         });
-        
-        
     }
-    
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
